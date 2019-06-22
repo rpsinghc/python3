71ece63dcd80a359165fe3adcd1f6a1489d91128
Ram Prakash Singh <rpsinghc@gmail.com>
2019-06-22 15:47:15 +0530

    moving to github

---
+++ src/helloworld/display/display.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import math
+import platform
+
+
+def disp():
+	# content = help('modules')
+	# print(content)
+	print(platform.python_version())
+	# h1=help('sys')
+	# print(h1)
+	print ("hello world")
+
---
+++ src/helloworld/helloworld.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from display.display import disp
+import site
+
+def main():
+	pass
+	disp()
+	print(help(site))
+
+if __name__ == '__main__':
+	main()
